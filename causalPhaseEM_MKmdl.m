771d497901f3265dc29eb15a0eebd8a4e15faffc
Ani <awodeyar@uci.edu>
2021-06-07 12:51:36 -0400

    New outputs and new function
    Altered the cred interval estiamtion to not force symmetry and to return simply the range directly so that one needn't do a second estimate of the difference of phase bounds. Also added in code that, given parameters, estiamtes phase using Kalman filter and does the backward smoothing too!

--- causalPhaseEM_MKmdl.m
+++ causalPhaseEM_MKmdl.m
@@ -28,7 +28,7 @@
 % 3 Kalman filter with the latest parameter estiamtes
 % Last edit: Ani Wodeyar 10/19/2020
 
-function [phase,phaseBounds,allX_full,circstd,cosineSum] = causalPhaseEM_MKmdl(y,initParams)
+function [phase,phaseBounds,allX_full,phaseWidth,returnParams] = causalPhaseEM_MKmdl(y,initParams)
 
 freqs = initParams.freqs;
 Fs = initParams.Fs;
@@ -55,6 +55,11 @@ data = y(1:windowSize);
 % first run to set up parameters
 [omega, ampEst, allQ, R, stateVec, stateCov] = fit_MKModel_multSines(data,freqs, Fs,ampVec, sigmaFreqs,sigmaObs);
 lowFreqLoc = find((omega>lowFreqBand(1)) & (omega<lowFreqBand(2)),1);
+returnParams.freqs = omega;
+returnParams.ampVec = ampEst;
+returnParams.sigmaFreqs = allQ;
+returnParams.sigmaObs = R;
+
  
 if isempty(lowFreqLoc)
     disp('Low freq band limits incorrect OR there is no low freq signal; retaining initial params')
@@ -71,8 +76,7 @@ end
 phase = zeros(numSegments, windowSize);
 phaseBounds = zeros(numSegments, windowSize,2);
 allX_full = zeros(numSegments, windowSize, 2);
-circstd = zeros(numSegments,windowSize);
-cosineSum = zeros(numSegments,windowSize);
+phaseWidth = zeros(numSegments,windowSize);
 
 for seg = 2:numSegments
 %     tic
@@ -96,50 +100,21 @@ for seg = 2:numSegments
         phase(seg, i) = angle(x_new(lowFreqLoc*2-1) + 1i* x_new(lowFreqLoc*2));
         samples = mvnrnd(x_new(lowFreqLoc*2-1:lowFreqLoc*2),...
             P_new(lowFreqLoc*2-1:lowFreqLoc*2,lowFreqLoc*2-1:lowFreqLoc*2),2000);
-        % akin to what is done under Technique 2 on page 206 of FIsher 1993
-        % statistical analysis of circular data
+        
         sampleAngles = (angle(exp(1i*angle(samples(:,1) + 1i*samples(:,2)) - 1i*phase(seg,i)))); % removing mean
-        tmpAngleStd = (wrapTo2Pi((prctile(sampleAngles,97.5) - prctile(sampleAngles,2.5)))/2); %[0,pi/2]
-        phaseBounds(seg,i,:) = sort([(phase(seg,i)- tmpAngleStd), (phase(seg,i) + tmpAngleStd)]); % can have a range of [0,pi]
-        circstd(seg,i) = ang_var2dev(abs(mean(exp(1i*sampleAngles))));
-        cosineSum(seg,i) = mean(cos(2*(sampleAngles)));
+        lowerBnd = (prctile(sampleAngles,2.5));
+        upperBnd = (prctile(sampleAngles,97.5));
+        phaseBounds(seg,i,:) = sort([lowerBnd + (phase(seg,i)), ...
+                                     upperBnd + (phase(seg,i))]); % can have a range of [0,2pi]
+        phaseWidth(seg,i) = rad2deg(ang_var2dev(abs(mean(exp(1i*sampleAngles)))));
 
         % update state and state cov
         P = P_new;
         x = x_new;
     end
-    % ending it on N-1
-%     [x_new,P_new] = oneStepKFupdate_sspp(x,y_thisRun(i),phi,M,Q,R,P);
-%     allX(:,i+1) = x_new;
-%     allP(:,:,i+1) = P_new;
-%     
-%     % estimate phase
-%     phase(seg, i) = angle(x_new(lowFreqLoc*2-1) + 1i* x_new(lowFreqLoc*2));
-%     samples = mvnrnd(x_new(lowFreqLoc*2-1:lowFreqLoc*2), P_new(lowFreqLoc*2-1:lowFreqLoc*2,lowFreqLoc*2-1:lowFreqLoc*2),2000);
-%     sampleAngles = angle(exp(1i*angle(samples(:,1) + 1i*samples(:,2)) - 1i*phase(seg,i)));
-%     tmpAngleStd = (wrapTo2Pi((prctile(sampleAngles,97.5) - prctile(sampleAngles,2.5)))/2);
-%     phaseBounds(seg,i,:) = sort([(phase(seg,i)- tmpAngleStd), (phase(seg,i) + tmpAngleStd)]);
-%     circstd(seg,i) = ang_var2dev(abs(mean(exp(1i*sampleAngles))));%exp(1i*sampleAngles)
-%     cosineSum(seg,i) = mean(cos(2*(sampleAngles)));
     
-
     allX_full(seg,:,:) = allX(lowFreqLoc*2-1:lowFreqLoc*2,:)';
     
-    % update below to take in the updated x start and cov start 
-%    [freqs, ampVec, sigmaFreqs, R, stateVec, stateCov,] = fit_MKModel_multSines(y_thisRun,omega, Fs, ampEst, allQ, R);
-%    tmp = find((freqs>lowFreqBand(1)) & (freqs<lowFreqBand(2)),1);
-%    
-%     if isempty(tmp)
-%         disp('Low freq band limits incorrect OR there is no low freq signal; retaining old parameters')       
-%     else
-%         lowFreqLoc = tmp;
-%         omega = freqs;
-%         ampEst = ampVec;
-%         allQ = sigmaFreqs;
-%     end
-%    
-%    [phi, Q, M] = genParametersSoulatMdl_sspp(omega, Fs, ampEst, allQ);
-%     toc
 end
     
 
---
+++ estimateMKphase.m
@@ -0,0 +1,77 @@
+function [phase,phaseBounds,phaseWidth] = estimateMKphase(data,freqs, Fs,ampVec, sigmaFreqs,sigmaObs,lowFreqLoc)
+
+
+if isempty(sigmaFreqs)
+    sigmaFreqs = 0.1*ones(length(freqs),1);
+end
+if isempty(sigmaObs)
+    sigmaObs = 10;
+end
+y = data;
+ang_var2dev = @(v)(sqrt(-2*log(1-v)));
+
+% need to initialize all my parameters
+
+[phi, Q, M] = genParametersSoulatMdl_sspp(freqs, Fs, ampVec, sigmaFreqs); 
+R = sigmaObs;
+
+xstart = zeros(2*length(freqs),1);
+Pstart = .001 * eye(2*length(freqs));
+x = xstart;
+P = Pstart;
+
+%% run forward KF
+for i = 1:(length(y))
+    
+    [x_new,P_new] = oneStepKFupdate_sspp(x,y(i),phi,M,Q,R,P);
+    allX(:,i) = x_new;
+    allP(:,:,i) = P_new;
+    
+    P = P_new;
+    x = x_new;
+end
+
+% now we have P_new as P_N_N and P as P_(N-1)_(N-1)
+% same for x
+% can now run the fixed interval smoother
+
+%% run the fixed interval smoother across all data
+
+x_n = x_new;
+P_n = P_new;
+
+newAllX(:, length(y)) = allX(:,length(y));
+newAllP(:,:,length(y)) = allP(:,:,length(y));
+for i = length(y)-1:-1:1
+    [x_backone,P_backone, J_one] =  fixedIntervalSmoother_sspp(x, x_n, P, P_n, phi, Q);
+    x_n = x_backone;
+    P_n = P_backone;
+    x = allX(:,i);
+    P = squeeze(allP(:,:,i));    
+    
+    % estimate phase
+    phase(i+1) = angle(x_n(lowFreqLoc*2-1) + 1i* x_n(lowFreqLoc*2));
+    samples = mvnrnd(x_n(lowFreqLoc*2-1:lowFreqLoc*2),...
+    P_n(lowFreqLoc*2-1:lowFreqLoc*2,lowFreqLoc*2-1:lowFreqLoc*2),2000);
+    sampleAngles = (angle(exp(1i*angle(samples(:,1) + 1i*samples(:,2)) - 1i*phase(i+1)))); % removing mean
+    lowerBnd = (prctile(sampleAngles,2.5));
+    upperBnd = (prctile(sampleAngles,97.5));
+    phaseBounds(i+1,:) = sort([lowerBnd + (phase(i+1)), ...
+                                 upperBnd + (phase(i+1))]); % can have a range of [0,2pi]
+    phaseWidth(i+1) = rad2deg(ang_var2dev(abs(mean(exp(1i*sampleAngles)))));
+
+    
+    newAllX(:,i+1) = x_backone;
+    newAllP(:,:,i+1) = P_backone;
+    allJ(:,:,i+1) = J_one;
+end
+[x_backone,P_backone, J_one] =  fixedIntervalSmoother_sspp(x, x_n, P, P_n, phi, Q);
+newAllX(:,1) = x_backone;
+newAllP(:,:,1) = P_backone;
+allJ(:,:,1) = J_one;
+
+% need to estimate P_t_(t-1) for t = 1:N
+P_tmp = phi * squeeze(allP(:,:,end-1)) * phi' +Q;
+K = P_tmp * M * (1/(M' * P_tmp*M + R));
+
+
